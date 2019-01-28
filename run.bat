058912d191d8b3e54400ad27fccc6faba683323f
Nesterov Alexander <nesoldr@mail.ru>
2019-01-29 02:48:27 +0300

    Init 30

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@
 echo "##########################################"
 echo "mpi"
 echo "##########################################"
+dir build\modules\test_tasks\mpi
 mpiexec.exe -np 4 build\modules\test_tasks\mpi\%configuration%\mpi.exe
 echo "##########################################"
 echo "omp"
