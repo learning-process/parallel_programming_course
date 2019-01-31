087eb6d609d93c83e9a0ef3a314dca473a51759f
Alexander Nesterov <nesoldr@mail.ru>
2019-01-31 18:19:57 -0100

    Added tbb #2

--- modules/test_tasks/tbb/main.cpp
+++ modules/test_tasks/tbb/main.cpp
@@ -5,12 +5,11 @@
 
 int main(int argc, char* argv[]) {
     tbb::flow::graph g;
-    tbb::flow::continue_node< tbb::flow::continue_msg > hello( g,
-      []( const tbb::flow::continue_msg &) { std::cout << "Hello"; }
-    );
-    tbb::flow::continue_node< tbb::flow::continue_msg> world( g,
-      []( const tbb::flow::continue_msg &) { std::cout << " World\n"; }
-    );
+    tbb::flow::continue_node< tbb::flow::continue_msg > hello(g,
+        [](const tbb::flow::continue_msg &) { std::cout << "Hello"; });
+
+    tbb::flow::continue_node< tbb::flow::continue_msg> world(g,
+        [](const tbb::flow::continue_msg &) { std::cout << " World\n"; });
     tbb::flow::make_edge(hello, world);
     hello.try_put(tbb::flow::continue_msg());
     g.wait_for_all();
