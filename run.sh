739bd2233906fb6f25d5907dab3368a35ecaad7a
Nesterov Alexander <nesoldr@mail.ru>
2019-01-31 03:39:57 +0300

    Update travis

--- run.sh
+++ run.sh
@@ -2,9 +2,8 @@ echo "##########################################"
 echo "mpi"
 echo "##########################################"
 mpirun -np 2 ./build/modules/test_tasks/mpi/mpi
-
 echo "##########################################"
 echo "omp"
 echo "##########################################"
-export OMP_NUM_THREADS=4
-./build/modules/test_tasks/omp/omp
+FILE="/build/modules/test_tasks/omp/omp"
+if [ -f "${PWD}$FILE" ]; then .$FILE; else echo "File $FILE does not exist!"; fi
