1842f52a9c6f23e3f72ab43162141ceee366b5d4
Nesterov Alexander <nesoldr@mail.ru>
2019-01-31 04:01:01 +0300

    Update bash script

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
+function run_mpi_file {
+  if [ -f "${PWD}$1" ]; then mpirun -np 2 .$1; else echo "File $1 does not exist!"; fi
+}
+
+function run_omp_file {
+    if [ -f "${PWD}$1" ]; then .$1; else echo "File $1 does not exist!"; fi
+}
+
 echo "##########################################"
 echo "mpi"
 echo "##########################################"
-mpirun -np 2 ./build/modules/test_tasks/mpi/mpi
+run_mpi_file "/build/modules/test_tasks/mpi/mpi"
 echo "##########################################"
 echo "omp"
 echo "##########################################"
-FILE="/build/modules/test_tasks/omp/omp"
-if [ -f "${PWD}$FILE" ]; then .$FILE; else echo "File $FILE does not exist!"; fi
+run_omp_file "/build/modules/test_tasks/omp/omp"
