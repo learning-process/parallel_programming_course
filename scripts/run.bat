b6f6da14ff80df9f0b60b64ff991cccb14495d3b
Alexander Nesterov <nesoldr@mail.ru>
2019-02-21 20:52:29 +0300

    Update batch script

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,4 +1,21 @@
 @echo off
-for /r "." %%a in (build\bin\*_mpi.exe) do mpiexec -np 4 %%~fa
-for /r "." %%a in (build\bin\*_omp.exe) do %%~fa
-for /r "." %%a in (build\bin\*_tbb.exe) do %%~fa
+for /r "." %%a in (build\bin\*_mpi.exe) do (
+    echo -------------------------------------
+    echo %%~na
+    echo -------------------------------------
+    mpiexec -np 4 %%~fa
+)
+
+for /r "." %%a in (build\bin\*_omp.exe) do (
+    echo -------------------------------------
+    echo %%~na
+    echo -------------------------------------
+    %%~fa
+)
+
+for /r "." %%a in (build\bin\*_tbb.exe) do (
+    echo -------------------------------------
+    echo %%~na
+    echo -------------------------------------
+    %%~fa
+)
