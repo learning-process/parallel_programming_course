ac8fdaf36381c39389a81cf7a05f2ec77c0f2efd
Nesterov Alexander <nesoldr@mail.ru>
2019-02-23 18:14:18 +0300

    Added flag for cmake

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 FILES_MPI="build/bin/*_mpi"
 for file in $FILES_MPI; do
     echo "--------------------------------"
-    echo $file
+    echo $(basename $file)
     echo "--------------------------------"
     mpirun -np 4 $file
 done
@@ -10,7 +10,7 @@ done
 FILES_OMP="build/bin/*_omp"
 for file in $FILES_OMP; do
     echo "--------------------------------"
-    echo $file
+    echo $(basename $file)
     echo "--------------------------------"
     ./$file
 done
@@ -18,7 +18,7 @@ done
 FILES_TBB="build/bin/*_tbb"
 for file in $FILES_TBB; do
     echo "--------------------------------"
-    echo $file
+    echo $(basename $file)
     echo "--------------------------------"
     ./$file
 done
