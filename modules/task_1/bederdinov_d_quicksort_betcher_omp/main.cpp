8a039031b7abd8e815a5ab3fa76bce1a520b31a8
Palidos <36964844+Palidos@users.noreply.github.com>
2019-03-10 14:28:18 +0300

    (Обновлено)Бедердинов Даниил. Задача 1. Сортировка Хоара с четно-нечетным слиянием Бэтчера. (#54)
    * initial commit
    
    * Fix whitespaces
    
    * Fix braces
    
    * Fix include
    
    * Fix lenth name
    
    * Add sorted array check
    
    * Fix array create
    
    * Delete my folder
    
    * Return my folder

--- modules/task_1/bederdinov_d_quicksort_betcher_omp/main.cpp
+++ modules/task_1/bederdinov_d_quicksort_betcher_omp/main.cpp
@@ -1,27 +1,38 @@
 // Copyright 2019 Bederdinov Daniil
-#define kLength 80
+#define kLength 100000
 #include <omp.h>
-#include <iostream>
-#include <ctime>
 #include <cmath>
+#include <ctime>
+#include <iostream>
 
-
-void CreateArray(int arr[], int length) {
+void shuffle(int *arr, int len) {
     srand((unsigned int)time(NULL));
-    for (int i = 0; i < length; i++) {
-        arr[i] = std::rand() % kLength;
+    int i = len, j, temp;
+    while (--i > 0) {
+        j = std::rand() % kLength;
+        temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
     }
 }
 
-void QuickSort(int *arr, int size) {
+void fillArray(int *arr, int len) {
+    for (int i = 0; i < len; i++) {
+        arr[i] = i;
+    }
+}
+
+void quickSort(int *arr, int size) {
     int i = 0, j = size - 1;
     int pivot = arr[size / 2];
 
     do {
-        while (arr[i] < pivot)
+        while (arr[i] < pivot) {
             i++;
-        while (arr[j] > pivot)
+        }
+        while (arr[j] > pivot) {
             j--;
+        }
 
         if (i <= j) {
             int tmp = arr[i];
@@ -34,27 +45,60 @@ void QuickSort(int *arr, int size) {
     } while (i <= j);
 
     if (j > 0)
-        QuickSort(arr, j + 1);
+        quickSort(arr, j + 1);
     if (i < size)
-        QuickSort(&arr[i], size - i);
+        quickSort(&arr[i], size - i);
+}
+
+bool check(int *A, int *B, int size) {
+    for (int i = 0; i < size; ++i)
+        if (A[i] == B[i]) {
+            return false;
+        }
+    return true;
+}
+
+int compare(const int *i, const int *j) {
+    return *i - *j;
 }
 
 int main(int argc, char *argv[]) {
-    int array[kLength];
-    CreateArray(array, kLength);
-    std::cout << std::endl;
-    std::cout << "array before sort:" << std::endl;
-    for (int i = 0; i < kLength; i++) {
-        std::cout << array[i] << " ";
+    int *copy = new int[kLength];
+    int *array = new int[kLength];
+
+    fillArray(array, kLength);
+    shuffle(array, kLength);
+
+    for (int i = 0; i < kLength; i++)
+        copy[i] = array[i];
+
+    if (kLength < 100) {
+        std::cout << std::endl;
+        std::cout << "Array before sort:" << std::endl;
+        for (int i = 0; i < kLength; i++) {
+            std::cout << array[i] << " ";
+        }
     }
 
-    std::cout << std::endl;
+    qsort(copy, kLength, sizeof(int), (int (*)(const void *, const void *))compare);
 
-    QuickSort(array, kLength);
-    std::cout << "array after sort:" << std::endl;
-    for (int i = 0; i < kLength; i++) {
-        std::cout << array[i] << " ";
-    }
     std::cout << std::endl;
+    quickSort(array, kLength);
+
+    if (kLength < 100) {
+        std::cout << "Array after sort:" << std::endl;
+        for (int i = 0; i < kLength; i++) {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    if (check(copy, array, kLength))
+        std::cout << "Arrays are equal" << std::endl;
+    else
+        std::cout << "Arrays are NOT equal" << std::endl;
+
+    delete[] copy;
+    delete[] array;
     return 0;
 }
