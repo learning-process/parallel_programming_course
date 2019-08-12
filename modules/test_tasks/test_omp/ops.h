89858be926648658ac47981cbfecdd8b95e65d47
Alexander Nesterov <nesoldr@mail.ru>
2019-08-12 18:43:00 +0000

    Added update

--- modules/test_tasks/test_omp/ops.h
+++ modules/test_tasks/test_omp/ops.h
@@ -1,6 +1,6 @@
 // Copyright 2018 Nesterov Alexander
-#ifndef MODULES_TEST_TASKS_OMP_OPS_H_
-#define MODULES_TEST_TASKS_OMP_OPS_H_
+#ifndef MODULES_TEST_TASKS_TEST_OMP_OPS_H_
+#define MODULES_TEST_TASKS_TEST_OMP_OPS_H_
 
 #include <vector>
 #include <string>
@@ -9,4 +9,4 @@ std::vector<int> getRandomVector(size_t sz);
 int getParallelOperations(std::vector<int> vec, std::string ops);
 int getSequentialOperations(std::vector<int> vec, std::string ops);
 
-#endif  // MODULES_TEST_TASKS_OMP_OPS_H_
+#endif  // MODULES_TEST_TASKS_TEST_OMP_OPS_H_
