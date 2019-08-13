98c2b26c24eeeda69fa80e53b511ae791912d7ba
Alexander Nesterov <nesoldr@mail.ru>
2019-08-13 18:47:04 +0000

    Added MPI 3

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,11 +1,4 @@
 @echo off
-for /r "." %%a in (build\bin\*_mpi.exe) do (
-    echo -------------------------------------
-    echo %%~na
-    echo -------------------------------------
-    mpiexec -np 4 %%~fa
-)
-
 for /r "." %%a in (build\bin\*_omp.exe) do (
     echo -------------------------------------
     echo %%~na
@@ -18,4 +11,11 @@ for /r "." %%a in (build\bin\*_tbb.exe) do (
     echo %%~na
     echo -------------------------------------
     %%~fa --gtest_repeat=10
+)
+
+for /r "." %%a in (build\bin\*_mpi.exe) do (
+    echo -------------------------------------
+    echo %%~na
+    echo -------------------------------------
+    for /L %a in (1,1,10) Do mpiexec -np 4 %%~fa
 )
