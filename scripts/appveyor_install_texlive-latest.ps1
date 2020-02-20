8994626c38b9b33d258238b2aa6b26e3af6bfa9d
Alexander Nesterov <nesoldr@mail.ru>
2020-02-20 17:25:36 -0100

    Added imagemagic 12

--- scripts/appveyor_install_texlive-latest.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$scriptRoot = (Resolve-Path $(If ($PSScriptRoot) { $PSScriptRoot } Else { "." })).Path
-. "${scriptRoot}\install_texlive_windows.ps1" `
-    -Repository 'http://mirrors.rit.edu/CTAN/systems/texlive/tlnet' `
-    -ProfilePath "${scriptRoot}\texlive2019-win.profile"
