676db61ac44e6f72d71a60462ee697323b444039
Nesterov Alexander <nesoldr@gmail.com>
2020-02-21 01:42:41 +0300

    Report: Latex (#17)
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Added report latex
    
    * Added report latex 2
    
    * Added report latex 3
    
    * Added report latex 4
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update .travis.yml
    
    * Update appveyor.yml
    
    * Added report latex 5
    
    * Added report latex win
    
    * Added imagemagic
    
    * Added imagemagic 2
    
    * Added imagemagic 3
    
    * Added imagemagic 4
    
    * Added imagemagic 5
    
    * Added imagemagic 6
    
    * Added imagemagic 7
    
    * Added imagemagic 8
    
    * Added imagemagic 9
    
    * Added imagemagic 10
    
    * Added imagemagic 11
    
    * Added imagemagic 12
    
    * Added imagemagic 13
    
    * Added imagemagic 14
    
    * Update README.md
    
    * Added MacOS support fol latex
    
    * Added MacOS support fol latex - FAILED
    
    * Update docs 1
    
    * Update docs 2

---
+++ scripts/appveyor_install_miktex-latest-minimal.ps1
@@ -0,0 +1,47 @@
+$ErrorActionPreference = "Stop"
+
+$scriptRoot = (Resolve-Path $(If ($PSScriptRoot) { $PSScriptRoot } Else { "." })).Path
+
+$installerPath = "$scriptRoot\basic-miktex-x64.exe"
+$installerUrl = 'http://mirrors.rit.edu/CTAN/systems/win32/miktex/setup/windows-x64/basic-miktex-2.9.7342-x64.exe'
+
+(New-Object System.Net.WebClient).DownloadFile($installerUrl, $installerPath)
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $installerPath
+$pinfo.Arguments = "--unattended --shared --auto-install=yes --paper-size=A4"
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.WorkingDirectory = $scriptRoot
+
+Write-Host "Starting MiKTeX installer"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+
+Write-Host "Waiting for MiKTeX installer to finish..."
+
+# Start async reads of output streams to avoid deadlock
+$p.BeginOutputReadLine()
+$p.BeginErrorReadLine()
+
+$p.WaitForExit()
+$exitCode = $p.ExitCode
+Write-Host "MiKTeX installer exited with code $exitCode"
+
+if ($exitCode -ne 0)
+{
+  throw "MiKTeX installer failed"
+}
+
+$env:Path = [System.Environment]::ExpandEnvironmentVariables(
+    [System.Environment]::GetEnvironmentVariable('Path', 'Machine') + ';' +
+    [System.Environment]::GetEnvironmentVariable('Path', 'User')
+)
+refreshenv
+
+initexmf --admin --enable-installer --verbose
+initexmf --admin --default-paper-size=a4 --verbose
+initexmf --admin --update-fndb --verbose
+initexmf --admin --mkmaps --verbose
