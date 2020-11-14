52bea1f0cb18e341ce97b6cebe8c4cabdeb08da0
Nesterov Alexander <nesoldr@mail.ru>
2020-11-15 01:31:26 +0300

    final update version cmake

--- scripts/install_latest_cmake.sh
+++ scripts/install_latest_cmake.sh
@@ -2,12 +2,6 @@ DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
 # shellcheck disable=SC2164
 mkdir "${DEPS_DIR}" && cd "${DEPS_DIR}"
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    travis_retry wget --no-check-certificate https://cmake.org/files/v3.18/cmake-3.18.0-Linux-x86_64.tar.gz
-    tar -xvf cmake-3.18.0-Linux-x86_64.tar.gz > /dev/null
-    sudo mv cmake-3.18.0-Linux-x86_64/CMake.app /Applications/CMake.app
-fi
-
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     travis_retry wget --no-check-certificate https://cmake.org/files/v3.18/cmake-3.18.0-Linux-x86_64.tar.gz
     tar -xvf cmake-3.18.0-Linux-x86_64.tar.gz > /dev/null
