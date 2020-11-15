cbaf0174f75eb8d3f948f5fda86780beb235583e
Nesterov Alexander <nesoldr@mail.ru>
2020-11-16 00:47:33 +0300

    refactor python scripts

--- scripts/lint.py
+++ scripts/lint.py
@@ -1,42 +1,51 @@
-import os, subprocess, re, sys
+import os
+import re
+import subprocess
+import sys
 
-######################################################################
 print("################ START FOUND LINT ERROR ################")
-def StartTests(command):
-    proc = subprocess.Popen(command, shell=True, stdout = subprocess.PIPE)
+
+
+def run(comm):
+    proc = subprocess.Popen(comm, shell=True, stdout=subprocess.PIPE)
     out = proc.stdout.read()
     proc.wait()
 
-    pattern = 'Total errors found: 0';
-    res_succ = re.findall(pattern, out)
+    pattern = 'Total errors found: 0'
+    res_success = re.findall(pattern, out)
+    if not res_success:
+        return False
+    return True
 
-    if len(res_succ) != 0: return True
-    else:                  return False
 
 pattern_filename = r'(\w+\.h|\w+\.cpp)$'
 
-project_directory = os.getcwd();
-home_directory = os.getcwd();
+project_directory = os.getcwd()
+home_directory = os.getcwd()
 
 cpplint_path = project_directory + '/3rdparty/cpplint.py'
 cpplint_path = os.path.abspath(cpplint_path)
 project_directory = os.path.abspath(project_directory)
 exit_flag = True
 for dirs, node, files in os.walk(project_directory):
-    for file in files:
-        if re.search(pattern_filename, file) != None:
-            if re.search(r'(task_1)|(task_2)|(task_3)|(task_4)|(test_task)', dirs) != None:
+    for curr_file in files:
+        if re.search(pattern_filename, curr_file) is not None:
+            regex_string = r'(task_1)|(task_2)|(task_3)|(task_4)|(test_task)'
+            if re.search(regex_string, dirs) is not None:
                 os.chdir(dirs)
-                print(dirs + " -> " + file)
-                command = sys.executable + ' ' + cpplint_path + ' --linelength=120 ' + file
-                status = StartTests(command)
+                print(dirs + " -> " + curr_file)
+                command = sys.executable + ' ' + cpplint_path + ' --linelength=120 ' + curr_file
+                status = run(command)
                 if status:
-                    print("\033[0;32mSuccess: " + file + "\033[0;0m");
+                    print("\033[0;32mSuccess: " + curr_file + "\033[0;0m")
                 else:
-                    print("\033[0;31mFailed: "  + file + "\033[0;0m");
+                    print("\033[0;31mFailed: " + curr_file + "\033[0;0m")
                     exit_flag = False
 os.chdir(home_directory)
+
 print("################ END FOUND LINT ERROR   ################")
-if exit_flag: exit(0)
-else: exit(1)
-######################################################################
+
+if exit_flag:
+    sys.exit(0)
+else:
+    sys.exit(1)
