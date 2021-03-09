34628c6d927b1758fcf45416c6885e21e1befa81
Nesterov Alexander <nesoldr@mail.ru>
2021-03-09 06:11:04 +0300

    Full valgrind upgrade

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,30 @@ ctest --extra-verbose --repeat-until-fail 10 --timeout 10 --build-and-test
 # shellcheck disable=SC2103
 cd ..
 
+FILES_SEQ="build/bin/*_seq"
+for file in $FILES_SEQ; do
+        echo "--------------------------------"
+        echo $(basename $file)
+        echo "--------------------------------"
+        valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./$file
+done
+
+FILES_OMP="build/bin/*_omp"
+for file in $FILES_OMP; do
+        echo "--------------------------------"
+        echo $(basename $file)
+        echo "--------------------------------"
+        valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./$file
+done
+
+FILES_TBB="build/bin/*_tbb"
+for file in $FILES_TBB; do
+        echo "--------------------------------"
+        echo $(basename $file)
+        echo "--------------------------------"
+        valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./$file
+done
+
 FILES_STD="build/bin/*_std"
 for file in $FILES_STD; do
         echo "--------------------------------"
