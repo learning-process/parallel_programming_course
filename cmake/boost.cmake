2b2e49f4e3aed1371c2dfbb76f9f652346222152
Alexander Nesterov <nesterov.alexander@outlook.com>
2022-12-28 16:41:40 +0100

    update windows 5

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -7,6 +7,13 @@ execute_process(
             -D CMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/boost-install
             -D BOOST_ENABLE_MPI=ON
             -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -D CMAKE_EXE_LINKER_FLAGS=${CMAKE_EXE_LINKER_FLAGS}
+            -D CMAKE_C_FLAGS_RELEASE=${CMAKE_C_FLAGS}
+            -D CMAKE_C_FLAGS_DEBUG=${CMAKE_C_FLAGS}
+            -D CMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+            -D CMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS}
+            -D CMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS}
+            -D CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
         RESULT_VARIABLE result)
 if(result)
     message(FATAL_ERROR "CMake step for boost failed: ${result}")
--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -6,6 +6,13 @@ execute_process(
         -B ${CMAKE_BINARY_DIR}/googletest-build
         -D CMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/googletest-install
         -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -D CMAKE_EXE_LINKER_FLAGS=${CMAKE_EXE_LINKER_FLAGS}
+        -D CMAKE_C_FLAGS_RELEASE=${CMAKE_C_FLAGS}
+        -D CMAKE_C_FLAGS_DEBUG=${CMAKE_C_FLAGS}
+        -D CMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+        -D CMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS}
+        -D CMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS}
+        -D CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
         RESULT_VARIABLE result)
 if(result)
     message(FATAL_ERROR "CMake step for googletest failed: ${result}")
