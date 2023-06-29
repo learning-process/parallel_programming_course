42eb4ca164b39152aa52b40b80247507c97af1bd
Nesterov Alexander <alexander.nesterov@intel.com>
2023-06-29 18:24:28 +0200

    Init performance architecture (#55)
    * Update README.md
    
    * change cppcheck dir
    
    * mini refactor
    
    * Update CMakeLists.txt
    
    * first arc of perf

---
+++ modules/core/src/perf.cpp
@@ -0,0 +1,41 @@
+// Copyright 2023 Nesterov Alexander
+#include <utility>
+#include <iostream>
+#include "core/include/perf.hpp"
+#include "./unapproved.h"
+
+ppc::core::Perf::Perf(std::shared_ptr<Task> task_) {
+    set_task(std::move(task_));
+}
+
+void ppc::core::Perf::set_task(std::shared_ptr<Task> task_) {
+    task = std::move(task_);
+}
+
+void ppc::core::Perf::pipeline_run(std::shared_ptr<PerfAttr> perfAttr,
+                                   std::shared_ptr<ppc::core::PerfResults> perfResults) {
+    common_run(std::move(perfAttr), [&]() {
+        task->pre_processing();
+        task->validation();
+        task->run();
+        task->post_processing();
+    }, std::move(perfResults));
+}
+
+void ppc::core::Perf::task_run(std::shared_ptr<PerfAttr> perfAttr,
+                               std::shared_ptr<ppc::core::PerfResults> perfResults) {
+    common_run(std::move(perfAttr), [&]() {
+        task->run();
+    }, std::move(perfResults));
+}
+
+void ppc::core::Perf::common_run(std::shared_ptr<PerfAttr> perfAttr, std::function<void()> pipeline,
+                                 std::shared_ptr<ppc::core::PerfResults> perfResults) {
+    auto begin = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < perfAttr->num_running; i++) {
+        pipeline();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+    perfResults->time_sec = duration * 1e-9;
+}
--- modules/core/tests/core_tests.cpp
+++ modules/core/tests/core_tests.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include "core/include/task.hpp"
 #include "core/tests/test_task.hpp"
+#include "core/include/perf.hpp"
 
 TEST(core_tests, check_int32_t) {
     // Create data
@@ -114,4 +115,34 @@ TEST(core_tests, check_float) {
     ASSERT_EQ(isValid, true);
     testTask.run();
     EXPECT_NEAR(out[0], in.size(), 1e-3);
+}
+
+TEST(core_tests, check_perf) {
+    // Create data
+    std::vector<uint16_t> in(2000, 1);
+    std::vector<uint16_t> out(1, 0);
+
+    // Create TaskData
+    auto taskData = std::make_shared<ppc::core::TaskData>();
+    taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+    taskData->inputs_count.emplace_back(in.size());
+    taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+    taskData->outputs_count.emplace_back(out.size());
+
+    // Create Task
+    auto testTask = std::make_shared<ppc::test::TestTask<uint16_t>>(taskData);
+
+    // Create Perf attributes
+    auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+    perfAttr->num_running = 10;
+
+    // Create and init perf results
+    auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+    // Create Perf analyzer
+    ppc::core::Perf perfAnalyzer(testTask);
+    perfAnalyzer.pipeline_run(perfAttr, perfResults);
+
+    ASSERT_LE(perfResults->time_sec, 10.0);
+    EXPECT_EQ(out[0], in.size());
 }
