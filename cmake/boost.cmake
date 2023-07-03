3d1f566e490e0084726f9e1f1abf00c95e8a99e1
Alexander Nesterov <nesterov.alexander@outlook.com>
2023-07-03 21:39:23 +0200

    update 3rdparty structure

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,35 +1,16 @@
 # Build Core Boost components
-execute_process(
-        COMMAND ${CMAKE_COMMAND}
-            -G ${CMAKE_GENERATOR}
-            -S ${CMAKE_SOURCE_DIR}/3rdparty/boost
-            -B ${CMAKE_BINARY_DIR}/boost-build
-            -D CMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/boost-install
-            -D BOOST_ENABLE_MPI=ON
-            -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-            -D CMAKE_EXE_LINKER_FLAGS=${CMAKE_EXE_LINKER_FLAGS}
-            -D CMAKE_C_FLAGS_RELEASE=${CMAKE_C_FLAGS}
-            -D CMAKE_C_FLAGS_DEBUG=${CMAKE_C_FLAGS}
-            -D CMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-            -D CMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS}
-            -D CMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS}
-            -D CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-            -D BOOST_INSTALL_LAYOUT=system
-        RESULT_VARIABLE result)
-if(result)
-    message(FATAL_ERROR "CMake step for boost failed: ${result}")
-endif()
-execute_process(
-        COMMAND ${CMAKE_COMMAND} --build "${CMAKE_BINARY_DIR}/boost-build" --config ${CMAKE_BUILD_TYPE} --parallel
-        RESULT_VARIABLE result)
-if(result)
-    message(FATAL_ERROR "Build step for boost failed: ${result}")
-endif()
-execute_process(
-        COMMAND ${CMAKE_COMMAND} --install "${CMAKE_BINARY_DIR}/boost-build"
-        RESULT_VARIABLE result)
-if(result)
-    message(FATAL_ERROR "Install step for boost failed: ${result}")
-endif()
+SUBDIRLIST(subdirs ${CMAKE_SOURCE_DIR}/3rdparty/boost/libs)
 
-include_directories(${CMAKE_BINARY_DIR}/boost-install/include)
+foreach(subd ${subdirs})
+        include_directories(${CMAKE_SOURCE_DIR}/3rdparty/boost/libs/${subd}/include)
+endforeach()
+
+add_subdirectory(${CMAKE_SOURCE_DIR}/3rdparty/boost)
+if( USE_MPI )
+        find_package( MPI )
+        if( MPI_FOUND )
+                add_subdirectory(${CMAKE_SOURCE_DIR}/3rdparty/boost/libs/mpi)
+        else( MPI_FOUND )
+                set( USE_MPI OFF )
+        endif( MPI_FOUND )
+endif( USE_MPI )
--- cmake/onetbb.cmake
+++ cmake/onetbb.cmake
@@ -1,34 +1,4 @@
 # Build Core OneTBB components
-execute_process(
-        COMMAND ${CMAKE_COMMAND}
-        -G ${CMAKE_GENERATOR}
-        -S ${CMAKE_SOURCE_DIR}/3rdparty/onetbb
-        -B ${CMAKE_BINARY_DIR}/onetbb-build
-        -D CMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/onetbb-install
-        -D TBB_TEST=OFF
-        -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -D CMAKE_EXE_LINKER_FLAGS=${CMAKE_EXE_LINKER_FLAGS}
-        -D CMAKE_C_FLAGS_RELEASE=${CMAKE_C_FLAGS}
-        -D CMAKE_C_FLAGS_DEBUG=${CMAKE_C_FLAGS}
-        -D CMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-        -D CMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS}
-        -D CMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS}
-        -D CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-        RESULT_VARIABLE result)
-if(result)
-    message(FATAL_ERROR "CMake step for onetbb failed: ${result}")
-endif()
-execute_process(
-        COMMAND ${CMAKE_COMMAND} --build "${CMAKE_BINARY_DIR}/onetbb-build" --config ${CMAKE_BUILD_TYPE} --parallel
-        RESULT_VARIABLE result)
-if(result)
-    message(FATAL_ERROR "Build step for onetbb failed: ${result}")
-endif()
-execute_process(
-        COMMAND ${CMAKE_COMMAND} --install "${CMAKE_BINARY_DIR}/onetbb-build"
-        RESULT_VARIABLE result)
-if(result)
-    message(FATAL_ERROR "Install step for onetbb failed: ${result}")
-endif()
-
-include_directories(${CMAKE_BINARY_DIR}/onetbb-install/include)
+option(TBB_TEST "Enable testing" OFF)
+include_directories(${CMAKE_SOURCE_DIR}/3rdparty/onetbb/include)
+add_subdirectory(${CMAKE_SOURCE_DIR}/3rdparty/onetbb)
