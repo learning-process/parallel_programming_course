1bde9ae41a11f026f6f4380f9ed2f0aa571c48b6
Nesterov Alexander <alexander.nesterov@intel.com>
2023-10-23 21:37:48 +0200

    Add samples (#58)
    * new modules
    
    * Add first sample
    
    * add limitation
    
    * add 3rdparty
    
    * add 3rdparty
    
    * 123

---
+++ first_samples/mpi_boost/main.cpp
@@ -0,0 +1,19 @@
+// Copyright 2023 Nesterov Alexander
+#include <mpi.h>
+#include <string>
+#include <iostream>
+#include <boost/mpi/environment.hpp>
+#include <boost/mpi/communicator.hpp>
+
+// https://www.boost.org/doc/libs/1_68_0/doc/html/mpi/tutorial.html
+int main(int argc, char** argv) {
+    boost::mpi::environment env(argc, argv);
+    boost::mpi::communicator world;
+
+    world.barrier();
+    std::cout << "Processor = " << boost::mpi::environment::processor_name() << std::endl;
+    std::cout << "Rank = " << world.rank() << std::endl;
+    std::cout << "Number of processors = " << world.size() << std::endl;
+
+    return 0;
+}
