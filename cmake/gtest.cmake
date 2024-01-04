82bc9c545051f2ddd24f5f7e3d8a7f937b33a405
Nesterov Alexander <nesterov.alexander@outlook.com>
2024-01-04 20:48:23 +0100

    fix win fails

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -8,5 +8,6 @@ ExternalProject_Add(ppc_googletest
         INSTALL_DIR       "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/install"
         CONFIGURE_COMMAND "${CMAKE_COMMAND}" -S "${CMAKE_SOURCE_DIR}/3rdparty/googletest/" -B "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/build/"
         -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -G${CMAKE_GENERATOR} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -Dgtest_force_shared_crt=ON
         BUILD_COMMAND     "${CMAKE_COMMAND}" --build "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/build" --config ${CMAKE_BUILD_TYPE}
         INSTALL_COMMAND   "${CMAKE_COMMAND}" --install "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/build" --prefix "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/install")
