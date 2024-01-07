bd4d64d105b59cad016cced1f6927fce2d0744e8
Alexander Nesterov <nesterov.alexander@outlook.com>
2024-01-07 03:28:02 +0100

    update perf limitation 3

--- modules/core/perf/include/perf.hpp
+++ modules/core/perf/include/perf.hpp
@@ -23,7 +23,7 @@ struct PerfResults {
   double time_sec = 0.0;
   enum TypeOfRunning { PIPELINE, TASK_RUN, NONE } type_of_running = NONE;
   constexpr const static double MAX_TIME = 10.0;
-  constexpr const static double MIN_TIME = 1.0;
+  constexpr const static double MIN_TIME = 0.05;
 };
 
 class Perf {
--- tasks/seq/example/src/ops_seq.cpp
+++ tasks/seq/example/src/ops_seq.cpp
@@ -5,7 +5,7 @@
 
 auto awake_time() {
   using std::chrono::operator"" ms;
-  return std::chrono::steady_clock::now() + 200ms;
+  return std::chrono::steady_clock::now() + 20ms;
 }
 
 bool TestTaskSequential::pre_processing() {
