25ee26748336ce957935db4a25e727860da0a89a
Arseniy Obolenskiy <gooddoog@student.su>
2024-01-07 12:52:53 +0800

    Fix more errors 2

--- tasks/seq/example/src/ops_seq.cpp
+++ tasks/seq/example/src/ops_seq.cpp
@@ -17,7 +17,7 @@ bool TestTaskSequential::validation() {
 
 bool TestTaskSequential::run() {
   internal_order_test();
-  for (size_t i = 0; i < input_; i++) {
+  for (int i = 0; i < input_; i++) {
     res++;
   }
   return true;
