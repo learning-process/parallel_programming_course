8b0a06fdbe31c67aa14f3f6de4feb7e8b760ef4c
Alexander Nesterov <nesterov.alexander@outlook.com>
2024-03-03 19:19:31 +0100

    check 1

--- scripts/generate_perf_results.sh
+++ scripts/generate_perf_results.sh
@@ -1,3 +1,3 @@
-source scripts/run_perf_collector.sh &> build/perf_log.txt
-python3 scripts/create_perf_table.py --input build/perf_log.txt --output build/bin
-rm build/perf_log.txt
+mkdir build/perf_stat_dir
+source scripts/run_perf_collector.sh &> build/perf_stat_dir/perf_log.txt
+python3 scripts/create_perf_table.py --input build/perf_stat_dir/perf_log.txt --output build/perf_stat_dir
