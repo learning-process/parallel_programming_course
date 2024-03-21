043f4b65ebbcbdc9c0ecdc43164cce9cefbf1259
Nesterov Alexander <alexander.nesterov@intel.com>
2024-03-21 01:49:56 +0100

    fix num checker (#105)

--- scripts/check_number_tests.py
+++ scripts/check_number_tests.py
@@ -26,6 +26,10 @@ for line, index in zip(lines, index_lines):
         tmp_name_test = line
 list_of_number_tests.append([tmp_name_test, abs(len(lines) - tmp_index) - 1])
 
+for elem in list_of_number_tests:
+    if elem[0] == "":
+        list_of_number_tests.remove(elem)
+
 type_of_test = ""
 if str(sys.argv[1]).find("func_tests") > 0:
     type_of_test = "func"
@@ -39,16 +43,14 @@ for res in list_of_number_tests:
             print("Project need to have more than or equal 5 tests")
             sys.exit(1)
         else:
-            if res[0] != "":
-                print("Project", res[0], "has", res[1], "tests: SUCCESS")
+            print("Project", res[0], "has", res[1], "tests: SUCCESS")
     elif type_of_test == "perf":
         if res[1] != 2:
             print("ERROR:", res[0], "has", res[1], "tests")
             print("Project need to have 2 tests")
             sys.exit(1)
         else:
-            if res[0] != "":
-                print("Project", res[0], "has", res[1], "tests: SUCCESS")
+            print("Project", res[0], "has", res[1], "tests: SUCCESS")
     else:
         print("Fatal error!")
         sys.exit(1)
