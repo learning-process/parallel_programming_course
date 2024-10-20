a3d5700cb05787aa7d7083be16baab3d503d5f75
Arseniy Obolenskiy <gooddoog@student.su>
2024-10-20 12:14:48 +0800

    Add error message for clang-tidy failure

--- 1stsamples/mpi/main.cpp
+++ 1stsamples/mpi/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char** argv) {
 
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  int y = 1 / 0;
 
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
