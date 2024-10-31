0e0296e6fd65ac9a440a03f4da8825df5c9a0f11
Arseniy Obolenskiy <gooddoog@student.su>
2024-11-01 00:44:44 +0800

    Run CI workloads on different OMP_NUM_THREADS configurations (from 1 thread to 16)

--- scripts/run_threads.sh
+++ scripts/run_threads.sh
@@ -10,14 +10,14 @@ if [[ $OSTYPE == "linux-gnu" && -z "$ASAN_RUN" ]]; then
 #  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/tbb_func_tests
 fi
 
-./build/bin/core_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
-./build/bin/ref_func_tests  --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
+./build/bin/core_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+./build/bin/ref_func_tests  --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
 
 ./build/bin/sample_omp
 ./build/bin/sample_stl
 ./build/bin/sample_tbb
 
-./build/bin/omp_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
-./build/bin/seq_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
-./build/bin/stl_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
-./build/bin/tbb_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
+./build/bin/omp_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+./build/bin/seq_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+./build/bin/stl_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+./build/bin/tbb_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
