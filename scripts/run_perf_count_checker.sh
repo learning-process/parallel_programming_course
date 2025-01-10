b7c552f5545e78e22ab836ba4d287e46331b52b9
Alexander Nesterov <alexander.nesterov@intel.com>
2025-01-10 18:13:13 +0100

    unite functions

--- scripts/run_perf_count_checker.sh
+++ scripts/run_perf_count_checker.sh
@@ -14,17 +14,6 @@ get_ref_tests_number() {
   echo "$ref_tests_number"
 }
 
-# shellcheck disable=SC2034
-MPI_REF_NUM="$(get_ref_tests_number "tasks/mpi" 3)"
-# shellcheck disable=SC2034
-OMP_REF_NUM="$(get_ref_tests_number "tasks/omp" 3)"
-# shellcheck disable=SC2034
-SEQ_REF_NUM="$(get_ref_tests_number "tasks/seq" 3)"
-# shellcheck disable=SC2034
-STL_REF_NUM="$(get_ref_tests_number "tasks/stl" 3)"
-# shellcheck disable=SC2034
-TBB_REF_NUM="$(get_ref_tests_number "tasks/tbb" 3)"
-
 get_current_tests_number() {
   # shellcheck disable=SC2155
   local ref_tests_number="$("$1" --gtest_list_tests | wc -l)"
@@ -33,44 +22,28 @@ get_current_tests_number() {
   echo "$ref_tests_number"
 }
 
-MPI_CURR_NUM="$(get_current_tests_number "./build/bin/mpi_perf_tests")"
-OMP_CURR_NUM="$(get_current_tests_number "./build/bin/omp_perf_tests")"
-SEQ_CURR_NUM="$(get_current_tests_number "./build/bin/seq_perf_tests")"
-STL_CURR_NUM="$(get_current_tests_number "./build/bin/stl_perf_tests")"
-TBB_CURR_NUM="$(get_current_tests_number "./build/bin/tbb_perf_tests")"
-
 get_error_msg() {
   echo "Count of perf tests in $1 != $REQUIRED_TESTS_COUNT"
 }
 
-if [[ MPI_REF_NUM -ne MPI_CURR_NUM ]]; then
-  # shellcheck disable=SC2005
-  echo "$(get_error_msg MPI)"
-  exit 1
-fi
+run_check() {
+  # shellcheck disable=SC2155
+  # shellcheck disable=SC2004
+  local ref_num="$(get_ref_tests_number "tasks/$1" $(($REQUIRED_TESTS_COUNT+1)))"
+  # shellcheck disable=SC2155
+  local curr_num="$(get_current_tests_number "./build/bin/$1_perf_tests")"
 
-if [[ OMP_REF_NUM -ne OMP_CURR_NUM ]]; then
-  # shellcheck disable=SC2005
-  echo "$(get_error_msg OMP)"
-  exit 1
-fi
+  if [[ ref_num -ne curr_num ]]; then
+    # shellcheck disable=SC2005
+    echo "$(get_error_msg "$1")"
+    exit 1
+  fi
+}
 
-if [[ SEQ_REF_NUM -ne SEQ_CURR_NUM ]]; then
-  # shellcheck disable=SC2005
-  echo "$(get_error_msg SEQ)"
-  exit 1
-fi
-
-if [[ STL_REF_NUM -ne STL_CURR_NUM ]]; then
-  # shellcheck disable=SC2005
-  echo "$(get_error_msg STL)"
-  exit 1
-fi
-
-if [[ TBB_REF_NUM -ne TBB_CURR_NUM ]]; then
-  # shellcheck disable=SC2005
-  echo "$(get_error_msg TBB)"
-  exit 1
-fi
+run_check "mpi"
+run_check "omp"
+run_check "seq"
+run_check "stl"
+run_check "tbb"
 
 echo "Success - perf count tests"
