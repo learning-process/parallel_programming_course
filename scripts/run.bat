e3c00df9dc93a89d81a49e8b11c65876315c76a7
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-01-12 23:49:09 +0100

    fix scripts

--- scripts/run.bat
+++ scripts/run.bat
@@ -9,7 +9,9 @@ if "%CLANG_BUILD%" NEQ "1" build\bin\sample_omp.exe
 build\bin\sample_stl.exe
 build\bin\sample_tbb.exe
 
+if "%CLANG_BUILD%" NEQ "1" mpiexec.exe -np 4 build\bin\all_func_tests.exe --gtest_repeat=10 || exit 1
 if "%CLANG_BUILD%" NEQ "1" mpiexec.exe -np 4 build\bin\mpi_func_tests.exe --gtest_repeat=10 || exit 1
+if "%CLANG_BUILD%" NEQ "1" build\bin\all_func_tests.exe || exit 1
 if "%CLANG_BUILD%" NEQ "1" build\bin\mpi_func_tests.exe || exit 1
 if "%CLANG_BUILD%" NEQ "1" build\bin\omp_func_tests.exe  --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating || exit 1
 build\bin\seq_func_tests.exe  --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating || exit 1
