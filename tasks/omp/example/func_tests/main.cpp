a8c0499351b8ba3ac66c9bcfce7d479ae2525f14
Nesterov Alexander <alexander.nesterov@intel.com>
2025-01-13 00:33:25 +0100

    Add "data" directory (#204)

--- tasks/omp/example/func_tests/main.cpp
+++ tasks/omp/example/func_tests/main.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <fstream>
 #include <random>
 #include <vector>
 
@@ -172,6 +173,51 @@ TEST(Parallel_Operations_OpenMP, Test_Mult) {
 
 TEST(Parallel_Operations_OpenMP, Test_Mult_2) {
   std::vector<int> vec = GetRandomVector(5);
+  // Create data
+  std::vector<int> ref_res(1, 0);
+
+  // Create TaskData
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(vec.data()));
+  task_data_seq->inputs_count.emplace_back(vec.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(ref_res.data()));
+  task_data_seq->outputs_count.emplace_back(ref_res.size());
+
+  // Create Task
+  nesterov_a_test_task_omp::TestOMPTaskSequential test_omp_task_sequential(task_data_seq, "*");
+  ASSERT_EQ(test_omp_task_sequential.validation(), true);
+  test_omp_task_sequential.pre_processing();
+  test_omp_task_sequential.run();
+  test_omp_task_sequential.post_processing();
+
+  // Create data
+  std::vector<int> par_res(1, 0);
+
+  // Create TaskData
+  auto task_data_par = std::make_shared<ppc::core::TaskData>();
+  task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t *>(vec.data()));
+  task_data_par->inputs_count.emplace_back(vec.size());
+  task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(par_res.data()));
+  task_data_par->outputs_count.emplace_back(par_res.size());
+
+  // Create Task
+  nesterov_a_test_task_omp::TestOMPTaskParallel test_omp_task_parallel(task_data_par, "*");
+  ASSERT_EQ(test_omp_task_parallel.validation(), true);
+  test_omp_task_parallel.pre_processing();
+  test_omp_task_parallel.run();
+  test_omp_task_parallel.post_processing();
+  ASSERT_EQ(ref_res[0], par_res[0]);
+}
+
+TEST(Parallel_Operations_OpenMP, Test_Mult_2_File) {
+  std::string line;
+  std::ifstream test_file(ppc::core::GetAbsolutePath("omp/example/data/test.txt"));
+  if (test_file.is_open()) {
+    getline(test_file, line);
+  }
+  test_file.close();
+
+  std::vector<int> vec = GetRandomVector(std::stoi(line));
   // Create data
   std::vector<int> ref_res(1, 0);
 
