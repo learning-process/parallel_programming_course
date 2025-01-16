43905ea7c92036b8049c3f74ee19ba55c635bbe1
Nesterov Alexander <alexander.nesterov@intel.com>
2025-01-16 06:45:43 +0100

    Add Win Ninja (MSVC and Clang) (#208)
    ... and rebalanced machines tree by load and execution duration

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -21,13 +21,21 @@ set( CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin" )
 set( CMAKE_CXX_STANDARD 20 )
 
 if( UNIX )
-    set(CMAKE_C_FLAGS  "${CMAKE_C_FLAGS} -Wall -Wextra -Wno-unused-parameter -Wno-cast-function-type -Wsign-compare -Werror")
-    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-unused-parameter -Wno-cast-function-type -Wsign-compare -Werror")
+    set(CMAKE_C_FLAGS  "${CMAKE_C_FLAGS}     \
+        -Wall -Wextra                        \
+        -Wno-unused-parameter                \
+        -Wno-cast-function-type              \
+        -Wsign-compare  -Werror")
+    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} \
+        -Wall -Wextra                        \
+        -Wno-unused-parameter                \
+        -Wno-cast-function-type              \
+        -Wsign-compare -Werror")
     if (USE_COVERAGE)
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
     endif(USE_COVERAGE)
-endif( UNIX )
+endif()
 
 if( MSVC )
     set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS}")
