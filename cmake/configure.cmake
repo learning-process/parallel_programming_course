fd83ecdeabd9d93b5b3d946d907bcb46db801bf0
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-01-16 01:29:34 +0100

    add exception

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -20,14 +20,25 @@ set( CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin" )
 
 set( CMAKE_CXX_STANDARD 20 )
 
-if( UNIX )
-    set(CMAKE_C_FLAGS  "${CMAKE_C_FLAGS} -Wall -Wextra -Wno-unused-parameter -Wno-cast-function-type -Wsign-compare -Werror")
-    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-unused-parameter -Wno-cast-function-type -Wsign-compare -Werror")
+string(FIND "${CMAKE_CXX_COMPILER_ID}" "Clang" CLANG_SUBSTR)
+if( UNIX OR (CLANG_SUBSTR GREATER -1) )
+    set(CMAKE_C_FLAGS  "${CMAKE_C_FLAGS}     \
+        -Wall -Wextra                        \
+        -Wno-unused-parameter                \
+        -Wno-cast-function-type              \
+        -Wno-unused-command-line-argument    \
+        -Wsign-compare  -Werror")
+    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} \
+        -Wall -Wextra                        \
+        -Wno-unused-parameter                \
+        -Wno-cast-function-type              \
+        -Wno-unused-command-line-argument    \
+        -Wsign-compare -Werror")
     if (USE_COVERAGE)
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
     endif(USE_COVERAGE)
-endif( UNIX )
+endif( UNIX OR (CLANG_SUBSTR GREATER -1) )
 
 if( MSVC )
     set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS}")
