777fd86e5a966dd03048e70425cc685fce5e085b
Arseniy Obolenskiy <gooddoog@student.su>
2025-01-25 23:54:20 +0100

    Implement PoC of build step cache for Ubuntu gcc

--- scripts/run_mpi.sh
+++ scripts/run_mpi.sh
@@ -2,17 +2,17 @@
 source build/ppc_opencv/install/bin/setup_vars_opencv4.sh
 
 if [[ $OSTYPE == "linux-gnu" && -z "$ASAN_RUN" ]]; then
-  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/core_func_tests
-  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/ref_func_tests
+  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all core_func_tests
+  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ref_func_tests
 
-#  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/all_func_tests
-#  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/mpi_func_tests
+#  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all all_func_tests
+#  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all mpi_func_tests
 fi
 
-./build/bin/core_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
-./build/bin/ref_func_tests  --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
+core_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
+ref_func_tests  --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
 
 if [[ -z "$ASAN_RUN" ]]; then
-    mpirun $1 -np $PROC_COUNT ./build/bin/all_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
-    mpirun $1 -np $PROC_COUNT ./build/bin/mpi_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
+    mpirun $1 -np $PROC_COUNT all_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
+    mpirun $1 -np $PROC_COUNT mpi_func_tests --gtest_also_run_disabled_tests --gtest_repeat=10 --gtest_recreate_environments_when_repeating
 fi
--- scripts/run_threads.sh
+++ scripts/run_threads.sh
@@ -2,19 +2,19 @@
 source build/ppc_opencv/install/bin/setup_vars_opencv4.sh
 
 if [[ $OSTYPE == "linux-gnu" && -z "$ASAN_RUN" ]]; then
-  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/core_func_tests
-  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/ref_func_tests
+  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all core_func_tests
+  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ref_func_tests
 
-#  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/omp_func_tests
-  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/seq_func_tests
-  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/stl_func_tests
-#  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./build/bin/tbb_func_tests
+#  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all omp_func_tests
+  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all seq_func_tests
+  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all stl_func_tests
+#  valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all tbb_func_tests
 fi
 
-./build/bin/core_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
-./build/bin/ref_func_tests  --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+core_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+ref_func_tests  --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
 
-./build/bin/omp_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
-./build/bin/seq_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
-./build/bin/stl_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
-./build/bin/tbb_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+omp_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+seq_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+stl_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
+tbb_func_tests --gtest_also_run_disabled_tests --gtest_repeat=3 --gtest_recreate_environments_when_repeating
