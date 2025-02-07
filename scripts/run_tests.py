3a259f39facd9c05e5e13798988cbf17fd9a6996
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-02-08 00:13:53 +0100

    funs to class

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -24,96 +24,97 @@ def init_cmd_args():
     return _args_dict
 
 
-def get_project_path():
-    script_path = Path(__file__).resolve()  # Absolute path of the script
-    script_dir = script_path.parent  # Directory containing the script
-    return script_dir.parent
+class PPCRunner:
+    def __init__(self):
+        self.work_dir = None
+        self.valgrind_cmd = "valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all"
+        self.ocv_script_name = "setup_vars_opencv4.sh"
+        self.ocv_script_path = "build/ppc_opencv/install/bin/" + self.ocv_script_name
 
+    def __get_project_path(self):
+        script_path = Path(__file__).resolve()  # Absolute path of the script
+        script_dir = script_path.parent  # Directory containing the script
+        return script_dir.parent
 
-def source_script(script_path):
-    command = f"bash -c 'source {script_path} && env'"
-    result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True)
-    if result.returncode == 0:
-        # Parse the output environment variables
-        env_vars = {}
-        for line in result.stdout.splitlines():
-            if '=' in line:
-                key, value = line.split("=", 1)
-                env_vars[key] = value
-        return env_vars
-    else:
-        print(f"Failed to source script: {script_path}")
-        return {}
+    @staticmethod
+    def __source_script(script_path):
+        command = f"bash -c 'source {script_path} && env'"
+        result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True)
+        if result.returncode == 0:
+            # Parse the output environment variables
+            env_vars = {}
+            for line in result.stdout.splitlines():
+                if '=' in line:
+                    key, value = line.split("=", 1)
+                    env_vars[key] = value
+            return env_vars
+        else:
+            print(f"Failed to source script: {script_path}")
+            return {}
 
+    def setup_env(self):
+        if os.path.isfile(Path(self.__get_project_path()) / self.ocv_script_path):
+            _work_dir = Path(self.__get_project_path()) / "build/bin"
+            env_vars = self.__source_script(Path(self.__get_project_path()) / self.ocv_script_path)
+        else:
+            _work_dir = Path(self.__get_project_path()) / "install/bin"
+            env_vars = self.__source_script(Path(_work_dir) / self.ocv_script_name)
 
-def setup_env():
-    ocv_script_path = "build/ppc_opencv/install/bin/setup_vars_opencv4.sh"
-    if os.path.isfile(Path(get_project_path()) / ocv_script_path):
-        _executable_path = Path(get_project_path()) / "build/bin"
-        env_vars = source_script(Path(get_project_path()) / ocv_script_path)
-    else:
-        _executable_path = Path(get_project_path()) / "install/bin"
-        env_vars = source_script(Path(_executable_path) / "setup_vars_opencv4.sh")
+        self.work_dir = Path(_work_dir)
+        os.environ.update(env_vars)
 
-    os.environ.update(env_vars)
-    return _executable_path
+    @staticmethod
+    def __run_exec(command):
+        result = subprocess.run(command, shell=True, env=os.environ)
+        if result.returncode != 0:
+            raise Exception(f"Subprocess return {result.returncode}.")
 
+    @staticmethod
+    def __get_gtest_settings(repeats_count):
+        command = "--gtest_also_run_disabled_tests "
+        command += f"--gtest_repeat={repeats_count} "
+        command += "--gtest_recreate_environments_when_repeating "
+        command += "--gtest_color=0 "
+        return command
 
-def run_exec_file(command):
-    result = subprocess.run(command, shell=True, env=os.environ)
-    if result.returncode != 0:
-        raise Exception(f"Subprocess return {result.returncode}.")
+    def run_threads(self):
+        if platform.system() == "Linux" and not os.environ.get("ASAN_RUN"):
+            self.__run_exec(f"{self.valgrind_cmd} {self.work_dir / 'seq_func_tests'} {self.__get_gtest_settings(1)}")
+            self.__run_exec(f"{self.valgrind_cmd} {self.work_dir / 'stl_func_tests'} {self.__get_gtest_settings(1)}")
 
+        self.__run_exec(f"{self.work_dir / 'omp_func_tests'} {self.__get_gtest_settings(3)}")
+        self.__run_exec(f"{self.work_dir / 'seq_func_tests'} {self.__get_gtest_settings(3)}")
+        self.__run_exec(f"{self.work_dir / 'stl_func_tests'} {self.__get_gtest_settings(3)}")
+        self.__run_exec(f"{self.work_dir / 'tbb_func_tests'} {self.__get_gtest_settings(3)}")
 
-def get_common_gtest_settings(repeats_count):
-    command = "--gtest_also_run_disabled_tests "
-    command += f"--gtest_repeat={repeats_count} "
-    command += "--gtest_recreate_environments_when_repeating "
-    command += "--gtest_color=0 "
-    return command
+    def run_core(self):
+        if platform.system() == "Linux" and not os.environ.get("ASAN_RUN"):
+            self.__run_exec(f"{self.valgrind_cmd} {self.work_dir / 'core_func_tests'} {self.__get_gtest_settings(1)}")
+            self.__run_exec(f"{self.valgrind_cmd} {self.work_dir / 'ref_func_tests'} {self.__get_gtest_settings(1)}")
 
+        self.__run_exec(f"{self.work_dir / 'core_func_tests'} {self.__get_gtest_settings(3)}")
+        self.__run_exec(f"{self.work_dir / 'ref_func_tests'}  {self.__get_gtest_settings(3)}")
 
-def run_threads(install_bin_dir):
-    valgrind_running = "valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all"
-    if platform.system() == "Linux" and not os.environ.get("ASAN_RUN"):
-        run_exec_file(f"{valgrind_running} {Path(install_bin_dir) / 'seq_func_tests'} {get_common_gtest_settings(1)}")
-        run_exec_file(f"{valgrind_running} {Path(install_bin_dir) / 'stl_func_tests'} {get_common_gtest_settings(1)}")
+    def run_processes(self, additional_mpi_args):
+        proc_count = os.environ.get("PROC_COUNT")
+        if proc_count is None:
+            raise EnvironmentError("Required environment variable 'PROC_COUNT' is not set.")
 
-    run_exec_file(f"{Path(install_bin_dir) / 'omp_func_tests'} {get_common_gtest_settings(3)}")
-    run_exec_file(f"{Path(install_bin_dir) / 'seq_func_tests'} {get_common_gtest_settings(3)}")
-    run_exec_file(f"{Path(install_bin_dir) / 'stl_func_tests'} {get_common_gtest_settings(3)}")
-    run_exec_file(f"{Path(install_bin_dir) / 'tbb_func_tests'} {get_common_gtest_settings(3)}")
-
-
-def run_core(install_bin_dir):
-    valgrind_running = "valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all"
-
-    if platform.system() == "Linux" and not os.environ.get("ASAN_RUN"):
-        run_exec_file(f"{valgrind_running} {Path(install_bin_dir) / 'core_func_tests'} {get_common_gtest_settings(1)}")
-        run_exec_file(f"{valgrind_running} {Path(install_bin_dir) / 'ref_func_tests'} {get_common_gtest_settings(1)}")
-
-    run_exec_file(f"{Path(install_bin_dir) / 'core_func_tests'} {get_common_gtest_settings(3)}")
-    run_exec_file(f"{Path(install_bin_dir) / 'ref_func_tests'}  {get_common_gtest_settings(3)}")
-
-
-def run_processes(install_bin_dir, additional_mpi_args):
-    proc_count = os.environ.get("PROC_COUNT")
-    if proc_count is None:
-        raise EnvironmentError("Required environment variable 'PROC_COUNT' is not set.")
-
-    mpi_running = f"mpirun {additional_mpi_args} -np {proc_count}"
-    if not os.environ.get("ASAN_RUN"):
-        run_exec_file(f"{mpi_running} {Path(install_bin_dir) / 'all_func_tests'} {get_common_gtest_settings(10)}")
-        run_exec_file(f"{mpi_running} {Path(install_bin_dir) / 'mpi_func_tests'} {get_common_gtest_settings(10)}")
+        mpi_running = f"mpirun {additional_mpi_args} -np {proc_count}"
+        if not os.environ.get("ASAN_RUN"):
+            self.__run_exec(f"{mpi_running} {self.work_dir / 'all_func_tests'} {self.__get_gtest_settings(10)}")
+            self.__run_exec(f"{mpi_running} {self.work_dir / 'mpi_func_tests'} {self.__get_gtest_settings(10)}")
 
 
 if __name__ == "__main__":
     args_dict = init_cmd_args()
-    executable_path = setup_env()
-    run_core(executable_path)
+
+    ppc_runner = PPCRunner()
+    ppc_runner.setup_env()
+    ppc_runner.run_core()
     if args_dict["running_type"] == "threads":
-        run_threads(executable_path)
+        ppc_runner.run_threads()
     elif args_dict["running_type"] == "processes":
-        run_processes(executable_path, args_dict["additional_mpi_args"])
+        ppc_runner.run_processes(args_dict["additional_mpi_args"])
     else:
         raise Exception("running-type is wrong!")
