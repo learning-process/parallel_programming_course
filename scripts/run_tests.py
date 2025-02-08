4a9306b3a1580760df1ab5afbd908f16f9050a72
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-02-08 03:38:45 +0100

    separate win msvc - 2

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -100,7 +100,7 @@ class PPCRunner:
         self.__run_exec(f"{self.work_dir / 'stl_func_tests'} {self.__get_gtest_settings(3)}")
         self.__run_exec(f"{self.work_dir / 'tbb_func_tests'} {self.__get_gtest_settings(3)}")
 
-        if os.environ.get("CLANG_BUILD") == 1:
+        if os.environ.get("CLANG_BUILD") == "1":
             return
         self.__run_exec(f"{self.work_dir / 'omp_func_tests'} {self.__get_gtest_settings(3)}")
 
@@ -113,7 +113,7 @@ class PPCRunner:
         self.__run_exec(f"{self.work_dir / 'ref_func_tests'}  {self.__get_gtest_settings(3)}")
 
     def run_processes(self, additional_mpi_args):
-        if os.environ.get("CLANG_BUILD") == 1:
+        if os.environ.get("CLANG_BUILD") == "1":
             return
 
         proc_count = os.environ.get("PROC_COUNT")
