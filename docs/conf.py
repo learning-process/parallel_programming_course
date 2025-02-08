00b8788906cae644107fb943cc88fcd1855e9c01
Arseniy Obolenskiy <gooddoog@student.su>
2025-02-08 20:56:27 +0100

    [docs] Enforce the same lest pane template in sphinx config
    Solve the problem when part of ToC disappears on the left pane on some pages

--- docs/conf.py
+++ docs/conf.py
@@ -25,3 +25,12 @@ gettext_compact = False
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_sidebars = {
+    "**": ["globaltoc.html", "sourcelink.html", "searchbox.html"]
+}
+
+html_theme_options = {
+    "collapse_navigation": False,
+    "navigation_depth": 2,
+}
