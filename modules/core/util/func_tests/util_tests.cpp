c613230d3bcca0541d1e96fb3f752e2da5673c40
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-02-09 23:10:53 +0100

    fix clang-tidy

--- modules/core/util/func_tests/util_tests.cpp
+++ modules/core/util/func_tests/util_tests.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <cstdlib>
+#include <string>
 #include <thread>
 
 #include "core/util/include/util.hpp"
@@ -8,11 +10,11 @@ TEST(util_tests, check_unset_env) {
 #ifndef _WIN32
   int save_var = ppc::util::GetPPCNumThreads();
 
-  unsetenv("OMP_NUM_THREADS");
+  unsetenv("OMP_NUM_THREADS");  // NOLINT(misc-include-cleaner)
 
   EXPECT_EQ(ppc::util::GetPPCNumThreads(), 1);
 
-  setenv("OMP_NUM_THREADS", std::to_string(save_var).c_str(), 1);
+  setenv("OMP_NUM_THREADS", std::to_string(save_var).c_str(), 1);  // NOLINT(misc-include-cleaner)
 #else
   GTEST_SKIP();
 #endif
@@ -23,11 +25,11 @@ TEST(util_tests, check_set_env) {
   int save_var = ppc::util::GetPPCNumThreads();
 
   const int num_threads = static_cast<int>(std::thread::hardware_concurrency());
-  setenv("OMP_NUM_THREADS", std::to_string(num_threads).c_str(), 1);
+  setenv("OMP_NUM_THREADS", std::to_string(num_threads).c_str(), 1);  // NOLINT(misc-include-cleaner)
 
   EXPECT_EQ(ppc::util::GetPPCNumThreads(), num_threads);
 
-  setenv("OMP_NUM_THREADS", std::to_string(save_var).c_str(), 1);
+  setenv("OMP_NUM_THREADS", std::to_string(save_var).c_str(), 1);  // NOLINT(misc-include-cleaner)
 #else
   GTEST_SKIP();
 #endif
