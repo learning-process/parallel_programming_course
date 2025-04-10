23552afd2bdc923f558c2b9b55eae20c573bad87
Arseniy Obolenskiy <gooddoog@student.su>
2025-04-10 10:25:00 +0200

    Enable 'pipefail' in scripts/generate_perf_results.sh (#362)
    Fail when run_tests.py script execution fails

--- scripts/generate_perf_results.sh
+++ scripts/generate_perf_results.sh
@@ -1,3 +1,5 @@
+set -o pipefail
+
 mkdir -p build/perf_stat_dir
 python3 scripts/run_tests.py --running-type="performance" | tee build/perf_stat_dir/perf_log.txt
 python3 scripts/create_perf_table.py --input build/perf_stat_dir/perf_log.txt --output build/perf_stat_dir
