85524063d15499c897b6ba235a2d506639b4aa3e
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-05-14 18:28:21 +0200

    change omp dir

--- tasks/omp/example/include/ops_omp.hpp
+++ tasks/omp/example/include/ops_omp.hpp
@@ -9,11 +9,14 @@ namespace nesterov_a_test_task_omp {
 
 class TestTaskOpenMP : public ppc::core::Task {
  public:
-  explicit TestTaskOpenMP(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
-  bool PreProcessingImpl() override;
+  explicit TestTaskOpenMP(const std::vector<int>& in) : input_(in) {}
   bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
+  std::vector<int> Get() {
+    return output_;
+  }
 
  private:
   std::vector<int> input_, output_;
