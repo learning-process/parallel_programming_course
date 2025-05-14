b8c80a53ce8fd190cb10ccf8e2c9f054cfb10387
Arseniy Obolenskiy <gooddoog@student.su>
2025-05-14 22:44:27 +0200

    Enable util_tests.cpp on Windows

--- modules/core/util/func_tests/util_tests.cpp
+++ modules/core/util/func_tests/util_tests.cpp
@@ -6,8 +6,15 @@
 
 #include "core/util/include/util.hpp"
 
+#ifdef _WIN32
+#include <Windows.h>
+inline int unsetenv(const char* name) { return SetEnvironmentVariableA(name, NULL) ? 0 : -1; }
+inline int setenv(const char* name, const char* value, int /*overwrite*/) {
+  return SetEnvironmentVariableA(name, value) ? 0 : -1;
+}
+#endif
+
 TEST(util_tests, check_unset_env) {
-#ifndef _WIN32
   int save_var = ppc::util::GetPPCNumThreads();
 
   unsetenv("PPC_NUM_THREADS");  // NOLINT(concurrency-mt-unsafe)
@@ -15,13 +22,9 @@ TEST(util_tests, check_unset_env) {
   EXPECT_EQ(ppc::util::GetPPCNumThreads(), 1);
 
   setenv("PPC_NUM_THREADS", std::to_string(save_var).c_str(), 1);  // NOLINT(concurrency-mt-unsafe)
-#else
-  GTEST_SKIP();
-#endif
 }
 
 TEST(util_tests, check_set_env) {
-#ifndef _WIN32
   int save_var = ppc::util::GetPPCNumThreads();
 
   const int num_threads = static_cast<int>(std::thread::hardware_concurrency());
@@ -30,7 +33,4 @@ TEST(util_tests, check_set_env) {
   EXPECT_EQ(ppc::util::GetPPCNumThreads(), num_threads);
 
   setenv("PPC_NUM_THREADS", std::to_string(save_var).c_str(), 1);  // NOLINT(concurrency-mt-unsafe)
-#else
-  GTEST_SKIP();
-#endif
 }
