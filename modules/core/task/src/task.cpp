677e6ea3799554b19494a6b9c58673d51b796e5c
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-05-16 02:02:02 +0200

    fix coverage 3

--- modules/core/task/src/task.cpp
+++ modules/core/task/src/task.cpp
@@ -1,8 +1,10 @@
 #include "core/task/include/task.hpp"
 
+#include <algorithm>
 #include <chrono>
 #include <cstddef>
 #include <exception>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
@@ -10,81 +12,90 @@
 #include <string>
 #include <utility>
 
+using namespace std::chrono;
+
 ppc::core::Task::Task(StateOfTesting state_of_testing) : state_of_testing_(state_of_testing) {
   functions_order_.clear();
 }
 
 bool ppc::core::Task::Validation() {
-  InternalOrderTest();
+  InternalOrderTest(__builtin_FUNCTION());
   return ValidationImpl();
 }
 
 bool ppc::core::Task::PreProcessing() {
-  InternalOrderTest();
+  InternalOrderTest(__builtin_FUNCTION());
+  if (state_of_testing_ == StateOfTesting::kFunc) {
+    InternalTimeTest(__builtin_FUNCTION());
+  }
   return PreProcessingImpl();
 }
 
 bool ppc::core::Task::Run() {
-  InternalOrderTest();
+  InternalOrderTest(__builtin_FUNCTION());
   return RunImpl();
 }
 
 bool ppc::core::Task::PostProcessing() {
-  InternalOrderTest();
+  InternalOrderTest(__builtin_FUNCTION());
+  if (state_of_testing_ == StateOfTesting::kFunc) {
+    InternalTimeTest(__builtin_FUNCTION());
+  }
   return PostProcessingImpl();
 }
 
-void ppc::core::Task::InternalOrderTest(const std::string& str) {
-  if (!functions_order_.empty() && str == functions_order_.back() && str == "Run") {
-    return;
-  }
-
+void ppc::core::Task::InternalOrderTest(const std::string &str) {
   functions_order_.push_back(str);
-
-  for (size_t i = 0; i < functions_order_.size(); i++) {
-    if (functions_order_[i] != right_functions_order_[i % right_functions_order_.size()]) {
-      functions_order_validation_ = false;
-      throw std::invalid_argument("ORDER OF FUNCTIONS IS NOT RIGHT: \n" + std::string("Serial number: ") +
-                                  std::to_string(i + 1) + "\n" + std::string("Your function: ") + functions_order_[i] +
-                                  "\n" + std::string("Expected function: ") + right_functions_order_[i]);
-    }
+  if (str == "PostProcessing" && IsFullPipelineStage()) {
+    functions_order_.clear();
+  } else {
+    was_worked_ = true;
   }
+}
 
-  if (str == "PreProcessing" && state_of_testing_ == StateOfTesting::kFunc) {
+void ppc::core::Task::InternalTimeTest(const std::string &str) {
+  if (str == "PreProcessing") {
     tmp_time_point_ = std::chrono::high_resolution_clock::now();
   }
 
-  if (str == "PostProcessing" && state_of_testing_ == StateOfTesting::kFunc) {
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - tmp_time_point_).count();
-    auto current_time = static_cast<double>(duration) * 1e-9;
+  if (str == "PostProcessing") {
+    auto duration = duration_cast<nanoseconds>(high_resolution_clock::now() - tmp_time_point_).count();
+    auto diff = static_cast<double>(duration) * 1e-9;
+
     std::stringstream err_msg;
-    if (current_time < kMaxTestTime) {
-      err_msg << "Test time:" << std::fixed << std::setprecision(10) << current_time;
+    if (diff < kMaxTestTime) {
+      err_msg << "Test time:" << std::fixed << std::setprecision(10) << diff;
     } else {
       err_msg << "\nTask execute time need to be: ";
       err_msg << "time < " << kMaxTestTime << " secs.\n";
-      err_msg << "Original time in secs: " << current_time << '\n';
+      err_msg << "Original time in secs: " << diff << '\n';
       throw std::runtime_error(err_msg.str().c_str());
     }
   }
 }
 
+bool ppc::core::Task::IsFullPipelineStage() {
+  auto it = std::adjacent_find(functions_order_.begin() + 2,
+                               functions_order_.begin() + static_cast<long>(functions_order_.size() - 2),
+                               std::not_equal_to<>());
+
+  return (functions_order_.size() >= 4 && functions_order_[0] == "Validation" &&
+          functions_order_[1] == "PreProcessing" && functions_order_[2] == "Run" &&
+          it == (functions_order_.begin() + static_cast<long>(functions_order_.size() - 2)) &&
+          functions_order_[functions_order_.size() - 1] == "PostProcessing");
+}
+
 ppc::core::Task::~Task() {
-  if (functions_order_.empty()) {
-    std::cerr << "ORDER OF FUNCTIONS IS NOT RIGHT: No task functions were executed\n";
+  auto custom_terminate = []() {
+    std::cerr << "ORDER OF FUNCTIONS IS NOT RIGHT! \n"
+                 "Expected - \"Validation\", \"PreProcessing\", \"Run\", \"PostProcessing\" \n";
+    std::exit(404);
+  };
+  std::set_terminate(custom_terminate);
+
+  if (!functions_order_.empty() || !was_worked_) {
     std::terminate();
+  } else {
+    functions_order_.clear();
   }
-  if (functions_order_validation_) {
-    for (size_t i = 0; i < functions_order_.size(); i++) {
-      if (functions_order_[i] != right_functions_order_[i % right_functions_order_.size()]) {
-        std::cerr << "ORDER OF FUNCTIONS IS NOT RIGHT: \n"
-                  << std::string("Serial number: ") << std::to_string(i + 1) << "\n"
-                  << std::string("Your function: ") << functions_order_[i] << "\n"
-                  << std::string("Expected function: ") << right_functions_order_[i] << "\n";
-        std::terminate();
-      }
-    }
-  }
-  functions_order_.clear();
 }
