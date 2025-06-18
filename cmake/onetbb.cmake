fcc341323e3d39a3807714ef58bae75ea93d24a7
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-06-18 11:17:47 +0200

    Refactor CMake scripts to replace `CONFIGURE_COMMAND` with `CMAKE_ARGS` for better clarity and maintainability.

--- cmake/onetbb.cmake
+++ cmake/onetbb.cmake
@@ -1,31 +1,43 @@
-# Build Core OneTBB components
-include_directories(${CMAKE_SOURCE_DIR}/3rdparty/onetbb/include)
-
 include(ExternalProject)
 
+include_directories("${CMAKE_SOURCE_DIR}/3rdparty/onetbb/include")
+
 if(WIN32)
-    set(ppc_onetbb_TEST_COMMAND "${CMAKE_COMMAND}" -E copy_directory "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/install/bin" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
+    set(ppc_onetbb_TEST_COMMAND "${CMAKE_COMMAND}" -E copy_directory
+            "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/install/bin"
+            "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
+    )
 else()
     set(ppc_onetbb_TEST_COMMAND "")
 endif()
 
 ExternalProject_Add(ppc_onetbb
-    SOURCE_DIR        "${CMAKE_SOURCE_DIR}/3rdparty/onetbb"
-    PREFIX            "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb"
-    BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/build"
-    INSTALL_DIR       "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/install"
-    CONFIGURE_COMMAND "${CMAKE_COMMAND}" -S "${CMAKE_SOURCE_DIR}/3rdparty/onetbb/" -B "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/build/"
-                      -DCMAKE_CXX_FLAGS="-w" -DCMAKE_C_FLAGS="-w"
-                      -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-                      -DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER} -DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}
-                      -G${CMAKE_GENERATOR} -DTBB_TEST=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-                      -D CMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD} -D CMAKE_CXX_STANDARD_REQUIRED=ON
-    BUILD_COMMAND     "${CMAKE_COMMAND}" --build "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/build" --config ${CMAKE_BUILD_TYPE} --parallel
-    INSTALL_COMMAND   "${CMAKE_COMMAND}" --install "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/build" --prefix "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/install"
-    TEST_COMMAND      ${ppc_onetbb_TEST_COMMAND}
+        SOURCE_DIR   "${CMAKE_SOURCE_DIR}/3rdparty/onetbb"
+        PREFIX       "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb"
+        BINARY_DIR   "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/build"
+        INSTALL_DIR  "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/install"
+
+        CMAKE_ARGS
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER}
+        -DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}
+        -DCMAKE_C_FLAGS=-w
+        -DCMAKE_CXX_FLAGS=-w
+        -DCMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
+        -DCMAKE_CXX_STANDARD_REQUIRED=ON
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DTBB_TEST=OFF
+
+        BUILD_COMMAND   "${CMAKE_COMMAND}" --build "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/build" --config ${CMAKE_BUILD_TYPE} --parallel
+        INSTALL_COMMAND "${CMAKE_COMMAND}" --install "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/build" --prefix "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/install"
+        TEST_COMMAND    ${ppc_onetbb_TEST_COMMAND}
 )
-install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/install/"
-        DESTINATION "${CMAKE_INSTALL_PREFIX}")
+
+install(
+        DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/ppc_onetbb/install/"
+        DESTINATION "${CMAKE_INSTALL_PREFIX}"
+)
 
 string(TOLOWER "${CMAKE_BUILD_TYPE}" cmake_build_type_lower)
 if(cmake_build_type_lower STREQUAL "debug")
