686006e7913dee9d362d3e3675cb5f3eb588acae
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-06-22 12:48:42 +0200

    log function names in task lifecycle methods using `ppc::util::FuncName()`

--- modules/core/task/include/task.hpp
+++ modules/core/task/include/task.hpp
@@ -108,6 +108,7 @@ class Task {
   /// @brief Validates input data and task attributes before execution.
   /// @return True if validation is successful.
   virtual bool Validation() final {
+    std::cout << ppc::util::FuncName() << '\n';
     InternalOrderTest(ppc::util::FuncName());
     return ValidationImpl();
   }
@@ -115,6 +116,7 @@ class Task {
   /// @brief Performs preprocessing on the input data.
   /// @return True if preprocessing is successful.
   virtual bool PreProcessing() final {
+    std::cout << ppc::util::FuncName() << '\n';
     InternalOrderTest(ppc::util::FuncName());
     if (state_of_testing_ == StateOfTesting::kFunc) {
       InternalTimeTest(ppc::util::FuncName());
@@ -125,6 +127,7 @@ class Task {
   /// @brief Executes the main logic of the task.
   /// @return True if execution is successful.
   virtual bool Run() final {
+    std::cout << ppc::util::FuncName() << '\n';
     InternalOrderTest(ppc::util::FuncName());
     return RunImpl();
   }
@@ -132,6 +135,7 @@ class Task {
   /// @brief Performs postprocessing on the output data.
   /// @return True if postprocessing is successful.
   virtual bool PostProcessing() final {
+    std::cout << ppc::util::FuncName() << '\n';
     InternalOrderTest(ppc::util::FuncName());
     if (state_of_testing_ == StateOfTesting::kFunc) {
       InternalTimeTest(ppc::util::FuncName());
