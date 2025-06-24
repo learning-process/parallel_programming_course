ac68a2e8c6746a0c5482fce5d4ab1de0f56cb1b5
Arseniy Obolenskiy <gooddoog@student.su>
2025-06-24 07:57:07 +0200

    Apply cmake format remarks

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -1,89 +1,91 @@
 if(MSVC)
-    option(gtest_force_shared_crt "" TRUE)
+  option(gtest_force_shared_crt "" TRUE)
 endif(MSVC)
 
-if (NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE "Release")
+if(NOT CMAKE_BUILD_TYPE)
+  set(CMAKE_BUILD_TYPE "Release")
 endif(NOT CMAKE_BUILD_TYPE)
 
-if (MSVC)
-    add_compile_options("/utf-8")
+if(MSVC)
+  add_compile_options("/utf-8")
 endif()
 
-set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/arch" )
-set( CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/lib" )
-set( CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/bin" )
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/arch")
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/lib")
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}/bin")
 
-set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/arch" )
-set( CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/lib" )
-set( CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/bin" )
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/arch")
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/lib")
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}/bin")
 
-set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/arch" )
-set( CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib" )
-set( CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin" )
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/arch")
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib")
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
 
-set( CMAKE_CXX_STANDARD 23 )
+set(CMAKE_CXX_STANDARD 23)
 
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 set(CMAKE_COMPILE_WARNING_AS_ERROR ON)
 
-if (USE_COVERAGE)
-    set(CMAKE_INSTALL_RPATH "${CMAKE_BINARY_DIR}/ppc_onetbb/install/lib")
+if(USE_COVERAGE)
+  set(CMAKE_INSTALL_RPATH "${CMAKE_BINARY_DIR}/ppc_onetbb/install/lib")
 else()
-    set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
-endif ()
+  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+endif()
 
 set(CMAKE_BUILD_WITH_INSTALL_RPATH ON)
 set(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON)
 set(CMAKE_SKIP_BUILD_RPATH OFF)
 
-if( UNIX )
-    set(COMMON_COMPILER_FLAGS
-        "${COMMON_COMPILER_FLAGS}            \
-        -Wall -Wextra                        \
-        -Wno-unused-parameter                \
-        -Wno-cast-function-type              \
+if(UNIX)
+  set(COMMON_COMPILER_FLAGS
+      "${COMMON_COMPILER_FLAGS}            \
+        -Wall -Wextra                      \
+        -Wno-unused-parameter              \
+        -Wno-cast-function-type            \
         -Wsign-compare")
-    if (NOT APPLE)
-        set(COMMON_COMPILER_FLAGS
-            "${COMMON_COMPILER_FLAGS}        \
-            -Wpedantic                       \
-            -Wpointer-arith                  \
-            -Wcast-align                     \
-            -Wwrite-strings                  \
-            -Wswitch-enum                    \
-            -Wnull-dereference               \
-            -Wswitch-enum                    \
-            -Wformat=2                       \
-            -Wmissing-declarations           \
+  if(NOT APPLE)
+    set(COMMON_COMPILER_FLAGS
+        "${COMMON_COMPILER_FLAGS}        \
+            -Wpedantic                   \
+            -Wpointer-arith              \
+            -Wcast-align                 \
+            -Wwrite-strings              \
+            -Wswitch-enum                \
+            -Wnull-dereference           \
+            -Wswitch-enum                \
+            -Wformat=2                   \
+            -Wmissing-declarations       \
             -Wno-c11-extensions")
-    endif (NOT APPLE)
-    set(CMAKE_C_FLAGS
-        "${CMAKE_C_FLAGS}                    \
-        -Wold-style-definition               \
+  endif(NOT APPLE)
+  set(CMAKE_C_FLAGS
+      "${CMAKE_C_FLAGS}                    \
+        -Wold-style-definition             \
         -Wmissing-prototypes")
 
-    if ("${ENABLE_ADDRESS_SANITIZER}" OR "${ENABLE_UB_SANITIZER}" OR "${ENABLE_LEAK_SANITIZER}")
-        set (COMMON_COMPILER_FLAGS "${COMMON_COMPILER_FLAGS} -Wno-cast-align")
-    endif()
+  if("${ENABLE_ADDRESS_SANITIZER}"
+     OR "${ENABLE_UB_SANITIZER}"
+     OR "${ENABLE_LEAK_SANITIZER}")
+    set(COMMON_COMPILER_FLAGS "${COMMON_COMPILER_FLAGS} -Wno-cast-align")
+  endif()
 
-    set(CMAKE_C_FLAGS  "${CMAKE_C_FLAGS} ${COMMON_COMPILER_FLAGS}")
-    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} ${COMMON_COMPILER_FLAGS}")
-    if (USE_COVERAGE)
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
-    endif(USE_COVERAGE)
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMMON_COMPILER_FLAGS}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMMON_COMPILER_FLAGS}")
+  if(USE_COVERAGE)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --coverage")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
+  endif(USE_COVERAGE)
 endif()
 
-if( MSVC )
-    set(COMMON_FLAGS "/W4 /wd4267 /wd4244")
-    set(CMAKE_C_FLAGS_RELEASE   "${CMAKE_C_FLAGS_RELEASE} ${COMMON_FLAGS}" )
-    set(CMAKE_C_FLAGS_DEBUG     "${CMAKE_C_FLAGS_DEBUG} ${COMMON_FLAGS}")
-    set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} ${COMMON_FLAGS}" )
-    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${COMMON_FLAGS}" )
-    set(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} ${COMMON_FLAGS}")
-    set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} ${COMMON_FLAGS}" )
-endif( MSVC )
+if(MSVC)
+  set(COMMON_FLAGS "/W4 /wd4267 /wd4244")
+  set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} ${COMMON_FLAGS}")
+  set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${COMMON_FLAGS}")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMMON_FLAGS}")
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${COMMON_FLAGS}")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${COMMON_FLAGS}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMMON_FLAGS}")
+endif(MSVC)
 
-find_package( Threads REQUIRED )
+find_package(Threads REQUIRED)
--- cmake/json.cmake
+++ cmake/json.cmake
@@ -2,21 +2,22 @@ include_directories(${CMAKE_SOURCE_DIR}/3rdparty/json/include)
 
 include(ExternalProject)
 
-ExternalProject_Add(ppc_json
-        SOURCE_DIR        "${CMAKE_SOURCE_DIR}/3rdparty/json"
-        PREFIX            "${CMAKE_CURRENT_BINARY_DIR}/ppc_json"
-        BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build"
-        INSTALL_DIR       "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/install"
-
-        CMAKE_ARGS
-        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-        -DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER}
-        -DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}
-        -DCMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
-        -DCMAKE_CXX_STANDARD_REQUIRED=ON
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-
-        BUILD_COMMAND     "${CMAKE_COMMAND}" --build "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build" --config ${CMAKE_BUILD_TYPE} --parallel
-        INSTALL_COMMAND   "${CMAKE_COMMAND}" --install "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build" --prefix "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/install"
-)
+ExternalProject_Add(
+  ppc_json
+  SOURCE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/json"
+  PREFIX "${CMAKE_CURRENT_BINARY_DIR}/ppc_json"
+  BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build"
+  INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/install"
+  CMAKE_ARGS -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+             -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+             -DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER}
+             -DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}
+             -DCMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
+             -DCMAKE_CXX_STANDARD_REQUIRED=ON
+             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+  BUILD_COMMAND
+    "${CMAKE_COMMAND}" --build "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build"
+    --config ${CMAKE_BUILD_TYPE} --parallel
+  INSTALL_COMMAND
+    "${CMAKE_COMMAND}" --install "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build"
+    --prefix "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/install")
