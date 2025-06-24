d4b9dd013a9c8e7f3b474452b7c326b99c0bd4a7
Nesterov Alexander <nesterov.alexander@outlook.com>
2025-06-24 18:37:12 +0200

    cmake: use modern OpenMP on MSVC

--- cmake/openmp.cmake
+++ cmake/openmp.cmake
@@ -1,6 +1,12 @@
 find_package(OpenMP)
 if(OpenMP_FOUND)
   include_directories(${OpenMP_C_INCLUDE_DIRS} ${OpenMP_CXX_INCLUDE_DIRS})
+
+  if(MSVC)
+    string(REPLACE "/openmp" "/openmp:llvm" OpenMP_C_FLAGS "${OpenMP_C_FLAGS}")
+    string(REPLACE "/openmp" "/openmp:llvm" OpenMP_CXX_FLAGS "${OpenMP_CXX_FLAGS}")
+  endif()
+
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
   set(CMAKE_EXE_LINKER_FLAGS
