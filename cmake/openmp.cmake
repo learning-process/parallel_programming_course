9d35fa20686846211ef46517b206918dfc618327
Nesterov Alexander <nesterov.alexander@outlook.com>
2025-06-25 10:44:33 +0200

    Update MSVC OpenMP flags (#480)
    - upgrade OpenMP flags on Windows MSVC to `/openmp:llvm`
    - ensure the modules use the same flag when linking OpenMP

--- cmake/openmp.cmake
+++ cmake/openmp.cmake
@@ -1,3 +1,19 @@
+if(MSVC AND NOT (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
+  set(OpenMP_C_FLAGS
+      "/openmp:llvm"
+      CACHE STRING "OpenMP C flags" FORCE)
+  set(OpenMP_CXX_FLAGS
+      "/openmp:llvm"
+      CACHE STRING "OpenMP CXX flags" FORCE)
+  # Pretend Clang-style OpenMP 5.0 support so CMake reports a newer version
+  set(OpenMP_C_SPEC_DATE
+      201811
+      CACHE STRING "OpenMP C specification date" FORCE)
+  set(OpenMP_CXX_SPEC_DATE
+      201811
+      CACHE STRING "OpenMP CXX specification date" FORCE)
+endif()
+
 find_package(OpenMP)
 if(OpenMP_FOUND)
   include_directories(${OpenMP_C_INCLUDE_DIRS} ${OpenMP_CXX_INCLUDE_DIRS})
