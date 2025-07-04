7c7dc2c784897b151738de69717b81b6ae63528f
Arseniy Obolenskiy <gooddoog@student.su>
2025-07-04 14:31:34 +0200

    Make run_tests.py executable

--- scripts/generate_perf_results.sh
+++ scripts/generate_perf_results.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 mkdir -p build/perf_stat_dir
-python3 scripts/run_tests.py --running-type="performance" | tee build/perf_stat_dir/perf_log.txt
+scripts/run_tests.py --running-type="performance" | tee build/perf_stat_dir/perf_log.txt
 python3 scripts/create_perf_table.py --input build/perf_stat_dir/perf_log.txt --output build/perf_stat_dir
