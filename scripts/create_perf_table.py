d05a949cd9441af823d1d9c31b399d4ce210bc27
Arseniy Obolenskiy <gooddoog@student.su>
2025-07-20 16:06:06 +0200

    test

--- scripts/create_perf_table.py
+++ scripts/create_perf_table.py
@@ -23,29 +23,76 @@ set_of_task_name = []
 logs_file = open(logs_path, "r")
 logs_lines = logs_file.readlines()
 for line in logs_lines:
-    pattern = r"tasks[\/|\\](\w*)[\/|\\](\w*):(\w*):(-*\d*\.\d*)"
-    result = re.findall(pattern, line)
-    if len(result):
-        task_name = result[0][1]
-        perf_type = result[0][2]
+    # Handle both old format: tasks/task_type/task_name:perf_type:time
+    # and new format: namespace_task_type_enabled:perf_type:time
+    old_pattern = r"tasks[\/|\\](\w*)[\/|\\](\w*):(\w*):(-*\d*\.\d*)"
+    new_pattern = (
+        r"(\w+_test_task_(threads|processes))_(\w+)_enabled:(\w*):(-*\d*\.\d*)"
+    )
+
+    old_result = re.findall(old_pattern, line)
+    new_result = re.findall(new_pattern, line)
+
+    if len(old_result):
+        task_name = old_result[0][1]
+        perf_type = old_result[0][2]
         set_of_task_name.append(task_name)
         result_tables[perf_type][task_name] = {}
 
         for ttype in list_of_type_of_tasks:
             result_tables[perf_type][task_name][ttype] = -1.0
+    elif len(new_result):
+        # Extract task name from namespace (e.g., "example_threads" from "nesterov_a_test_task_threads")
+        full_task_name = new_result[0][0]
+        task_category = new_result[0][1]  # "threads" or "processes"
+        task_name = f"example_{task_category}"
+        perf_type = new_result[0][3]
+
+        if task_name not in set_of_task_name:
+            set_of_task_name.append(task_name)
+
+        if perf_type not in result_tables:
+            result_tables[perf_type] = {}
+        if task_name not in result_tables[perf_type]:
+            result_tables[perf_type][task_name] = {}
+            for ttype in list_of_type_of_tasks:
+                result_tables[perf_type][task_name][ttype] = -1.0
 
 for line in logs_lines:
-    pattern = r"tasks[\/|\\](\w*)[\/|\\](\w*):(\w*):(-*\d*\.\d*)"
-    result = re.findall(pattern, line)
-    if len(result):
-        task_type = result[0][0]
-        task_name = result[0][1]
-        perf_type = result[0][2]
-        perf_time = float(result[0][3])
+    # Handle both old format: tasks/task_type/task_name:perf_type:time
+    # and new format: namespace_task_type_enabled:perf_type:time
+    old_pattern = r"tasks[\/|\\](\w*)[\/|\\](\w*):(\w*):(-*\d*\.\d*)"
+    new_pattern = (
+        r"(\w+_test_task_(threads|processes))_(\w+)_enabled:(\w*):(-*\d*\.\d*)"
+    )
+
+    old_result = re.findall(old_pattern, line)
+    new_result = re.findall(new_pattern, line)
+
+    if len(old_result):
+        task_type = old_result[0][0]
+        task_name = old_result[0][1]
+        perf_type = old_result[0][2]
+        perf_time = float(old_result[0][3])
         if perf_time < 0.1:
             msg = f"Performance time = {perf_time} < 0.1 second : for {task_type} - {task_name} - {perf_type} \n"
             raise Exception(msg)
         result_tables[perf_type][task_name][task_type] = perf_time
+    elif len(new_result):
+        # Extract task details from namespace format
+        full_task_name = new_result[0][0]
+        task_category = new_result[0][1]  # "threads" or "processes"
+        task_type = new_result[0][2]  # "all", "omp", "seq", etc.
+        perf_type = new_result[0][3]
+        perf_time = float(new_result[0][4])
+        task_name = f"example_{task_category}"
+
+        if perf_time < 0.1:
+            msg = f"Performance time = {perf_time} < 0.1 second : for {task_type} - {task_name} - {perf_type} \n"
+            raise Exception(msg)
+
+        if task_name in result_tables[perf_type]:
+            result_tables[perf_type][task_name][task_type] = perf_time
 
 
 for table_name in result_tables:
