77445462742104dfc119d3aa26449e2929974ca6
Arseniy Obolenskiy <gooddoog@student.su>
2025-08-01 17:24:27 +0200

    Use target_include_directories for json include path

--- cmake/json.cmake
+++ cmake/json.cmake
@@ -27,6 +27,6 @@ function(ppc_link_json exec_func_lib)
                              PUBLIC ${CMAKE_SOURCE_DIR}/3rdparty/json/include)
 
   add_dependencies(${exec_func_lib} ppc_json)
-  target_link_directories(${exec_func_lib} INTERFACE
-                          "${CMAKE_BINARY_DIR}/ppc_json/install/include")
+  target_include_directories(
+    ${exec_func_lib} INTERFACE "${CMAKE_BINARY_DIR}/ppc_json/install/include")
 endfunction()
