ee1b4bca1bdf65287c3441bcd2f35285d52e553b
anvar <mr.nazyrov1997@mail.ru>
2026-05-02 16:12:08 +0300

    fix: update task files

--- tasks/nazyrov_a_a_vector_avg/common/include/common.hpp
+++ tasks/nazyrov_a_a_vector_avg/common/include/common.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <string>
-#include <tuple>
+
 #include <vector>
 
 #include "task/include/task.hpp"
@@ -10,7 +9,7 @@ namespace nazyrov_a_a_vector_avg {
 
 using InType = std::vector<int>;
 using OutType = double;
-using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
+
 
 }  // namespace nazyrov_a_a_vector_avg
